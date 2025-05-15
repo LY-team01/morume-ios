a96e5b1da3069d11cf9ebb7d6acd0ed5ba9469f5
Hikaru Aohara <hikaruaohara@icloud.com>
2025-05-15 11:53:33 +0900

    Xcodeプロジェクトを作成

---
+++ Morume/Morume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Morume
+//
+//  Created by 青原光 on 2025/05/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Morume/Morume/MorumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorumeApp.swift
+//  Morume
+//
+//  Created by 青原光 on 2025/05/15.
+//
+
+import SwiftUI
+
+@main
+struct MorumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
