d3ceb6cb01a592c9bdbcd868c38c38413b08dff6
Hikaru Aohara <hikaruaohara@icloud.com>
2025-05-17 20:27:03 +0900

    フィルターを編集する画面のファイルを作成

---
+++ Morume/MoruMe/Features/FilterEdit/FilterEditView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterEditView.swift
+//  MoruMe
+//
+//  Created by 青原光 on 2025/05/17.
+//
+
+import SwiftUI
+
+struct FilterEditView: View {
+    var body: some View {
+        Text("FilterEditView")
+    }
+}
+
+#Preview {
+    FilterEditView()
+}
