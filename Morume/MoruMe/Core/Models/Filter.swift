8b5b8d0145c9c4cd594265a14b8fa7593f943d4e
Hikaru Aohara <hikaruaohara@icloud.com>
2025-05-17 16:57:54 +0900

    Filterモデルを作成

---
+++ Morume/MoruMe/Core/Models/Filter.swift
@@ -0,0 +1,15 @@
+//
+//  Filter.swift
+//  MoruMe
+//
+//  Created by 青原光 on 2025/05/17.
+//
+
+struct Filter: Codable {
+    let brightness: Int
+    let skin: Int
+    let contour: Int
+    let eye: Int
+    let nose: Int
+    let mouth: Int
+}
