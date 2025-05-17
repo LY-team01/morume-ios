0f5afb064ac531da0f3cd46654999fc042221455
Hikaru Aohara <hikaruaohara@icloud.com>
2025-05-17 20:24:08 +0900

    写真を加工する画面のファイルを作成

---
+++ Morume/MoruMe/Features/PhotoEdit/PhotoEditView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoEditView.swift
+//  MoruMe
+//
+//  Created by 青原光 on 2025/05/17.
+//
+
+import SwiftUI
+
+struct PhotoEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoEditView()
+}
