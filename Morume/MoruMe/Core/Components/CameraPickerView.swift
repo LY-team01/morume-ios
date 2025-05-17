9215dac76487a728ec7ec6f819c5a9d6e2ef03e7
Hikaru Aohara <hikaruaohara@icloud.com>
2025-05-17 20:48:19 +0900

    カメラを起動して写真を取得するUIViewControllerRepresentableを実装

---
+++ Morume/MoruMe/Core/Components/CameraPickerView.swift
@@ -0,0 +1,45 @@
+//
+//  CameraPickerView.swift
+//  MoruMe
+//
+//  Created by 青原光 on 2025/05/17.
+//
+
+import SwiftUI
+
+struct CameraPickerView: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: CameraPickerView
+
+        init(_ parent: CameraPickerView) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            picker.dismiss(animated: true)
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.selectedImage = nil
+            picker.dismiss(animated: true)
+        }
+    }
+}
