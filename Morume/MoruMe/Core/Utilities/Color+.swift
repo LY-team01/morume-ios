3d6bf59b8d807e0eb4e768b74c5c0e8f0c19fdcc
Hikaru Aohara <hikaruaohara@icloud.com>
2025-05-18 16:49:00 +0900

    カスタムカラーのColor拡張を実装

---
+++ Morume/MoruMe/Core/Utilities/Color+.swift
@@ -0,0 +1,20 @@
+//
+//  Color+.swift
+//  MoruMe
+//
+//  Created by 青原光 on 2025/05/18.
+//
+
+import SwiftUI
+
+extension Color {
+    static var morumeBlue: Color {
+        Color("MoruMeBlue")
+    }
+    static var morumeGreen: Color {
+        Color("MoruMeGreen")
+    }
+    static var morumePink: Color {
+        Color("MoruMePink")
+    }
+}
