3a10da9b795089fa9f7a9a1ab3c20accaeb35031
Hikaru Aohara <hikaruaohara@icloud.com>
2025-05-24 21:10:00 +0900

    FirebaseAuthServiceの実装

---
+++ Morume/MoruMe/Core/Services/Auth/AuthService.swift
@@ -0,0 +1,28 @@
+//
+//  AuthService.swift
+//  MoruMe
+//
+//  Created by 青原光 on 2025/05/24.
+//
+
+import FirebaseAuth
+
+protocol AuthService {
+    func login() async throws
+    func getToken() async throws -> String
+}
+
+final class FirebaseAuthService: AuthService {
+    private let auth = Auth.auth()
+
+    func login() async throws {
+        try await auth.signInAnonymously()
+    }
+
+    func getToken() async throws -> String {
+        guard let user = auth.currentUser else {
+            throw NSError(domain: "User not authenticated", code: 401, userInfo: nil)
+        }
+        return try await user.getIDToken()
+    }
+}
