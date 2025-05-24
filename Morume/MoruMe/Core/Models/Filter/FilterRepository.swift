006c2df6e5fdcc6841c94200bf5f6a60453e9271
Hikaru Aohara <hikaruaohara@icloud.com>
2025-05-24 23:46:34 +0900

    Filter.userIDはStringなので修正

--- Morume/MoruMe/Core/Models/Filter/FilterRepository.swift
+++ Morume/MoruMe/Core/Models/Filter/FilterRepository.swift
@@ -9,5 +9,5 @@ protocol FilterRepository {
     /// 自分のフィルターを取得
     func fetchMyFilter() async throws -> Filter
     /// フィルターを取得
-    func fetchFilter(userID: Int) async throws -> Filter
+    func fetchFilter(userID: String) async throws -> Filter
 }
