f014bfbec15c9d593701586b072c66c967ea573a
Hikaru Aohara <hikaruaohara@icloud.com>
2025-05-24 22:24:14 +0900

    API仕様の変更を反映

--- Morume/MoruMe/Core/Models/Filter/FilterRepository.swift
+++ Morume/MoruMe/Core/Models/Filter/FilterRepository.swift
@@ -6,8 +6,6 @@
 //
 
 protocol FilterRepository {
-    /// フィルターを登録
-    func putFilter(filter: Filter) async throws -> Filter
     /// 自分のフィルターを取得
     func fetchMyFilter() async throws -> Filter
     /// フィルターを取得
