b43a6154967a7e60dbf735b492756c06dd1177c8
nmotisk <taisuke.1214n@gmail.com>
2025-05-24 17:16:39 +0900

    ファイルに直接色を定義

--- Morume/MoruMe/Core/Components/MoruMeBackground.swift
+++ Morume/MoruMe/Core/Components/MoruMeBackground.swift
@@ -11,13 +11,13 @@ struct MoruMeBackground: View {
     var body: some View {
         LinearGradient(
             gradient: Gradient(colors: [
-                Color("GradientStartColor"),
-                Color("GradientEndColor")
+                Color(red: 217/255, green: 255/255, blue: 255/255),
+                Color(red: 255/255, green: 226/255, blue: 235/255)
             ]),
             startPoint: .topLeading,
         endPoint: .bottomTrailing
         )
-        .edgesIgnoringSafeArea(.all)
+        .ignoresSafeArea()
     }
 }
 
