017897d4e0c83c600c063b78a0fca66302f8f9d2
Hikaru Aohara <hikaruaohara@icloud.com>
2025-05-24 22:49:48 +0900

    AuthService.swiftを分割

--- Morume/MoruMe/Core/Services/Auth/AuthService.swift
+++ Morume/MoruMe/Core/Services/Auth/AuthService.swift
@@ -5,26 +5,7 @@
 //  Created by 青原光 on 2025/05/24.
 //
 
-import FirebaseAuth
-
 protocol AuthService {
     func login() async throws
     func getToken() async throws -> String
-}
-
-final class FirebaseAuthService: AuthService {
-    private let auth = Auth.auth()
-
-    func login() async throws {
-        if auth.currentUser == nil {
-            try await auth.signInAnonymously()
-        }
-    }
-
-    func getToken() async throws -> String {
-        guard let user = auth.currentUser else {
-            throw NSError(domain: "User not authenticated", code: 401, userInfo: nil)
-        }
-        return try await user.getIDToken(forcingRefresh: true)
-    }
 }
