625e19b47b0a4a24f0882363c5aa9b00d41fc6cb
Hikaru Aohara <hikaruaohara@icloud.com>
2025-05-24 20:35:06 +0900

    FirebaseAuthのためのAppDelegateを作成

--- Morume/MoruMe/MoruMeApp.swift
+++ Morume/MoruMe/MoruMeApp.swift
@@ -5,10 +5,25 @@
 //  Created by 青原光 on 2025/05/15.
 //
 
+import FirebaseAuth
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication
+            .LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct MoruMeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
