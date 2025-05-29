387510fb01bfb742a62ade18cd0f8e6120171ec1
Hikaru Aohara <hikaruaohara@icloud.com>
2025-05-30 02:08:52 +0900

    検出した顔の数だけのアイテムを持つリストを作成する処理の実装

--- Morume/MoruMe/Features/PhotoEdit/PhotoEditView.swift
+++ Morume/MoruMe/Features/PhotoEdit/PhotoEditView.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct PhotoEditView: View {
+    @State private var viewModel: PhotoEditViewModel
+
+    init(photo: UIImage) {
+        self.viewModel = PhotoEditViewModel(originalImage: photo)
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Color(uiColor: .systemGray6)
+                    .ignoresSafeArea()
+
+                VStack(spacing: 0) {
+                    photoDisplayArea
+
+                    userList
+                }
+            }
+        }
+        .task {
+            await viewModel.detectFaceLandmarks()
+        }
+    }
+
+    // MARK: editArea
+    private var photoDisplayArea: some View {
+        Image(uiImage: viewModel.editPhoto)
+            .resizable()
+            .scaledToFit()
+            .frame(maxWidth: .infinity, maxHeight: 350)
+    }
+
+    // MARK: userList
+    private var userList: some View {
+        List {
+            ForEach(viewModel.detectedFaces) { detectedFace in
+                Button {
+                    print("pushed")
+                } label: {
+                    HStack(spacing: 18) {
+                        RoundedRectangle(cornerRadius: 7)
+                            .fill(detectedFace.color)
+                            .frame(width: 30, height: 30)
+
+                        if let nickname = detectedFace.user?.nickname {
+                            Text(nickname)
+                        } else {
+                            Text("ユーザー未選択")
+                        }
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }
+                    .foregroundStyle(Color(uiColor: .systemGray2))
+                }
+            }
+        }
+        .scrollBounceBehavior(.basedOnSize)
+        .scrollContentBackground(.hidden)
     }
 }
 
 #Preview {
-    PhotoEditView()
+    let photo = UIImage(resource: .sampleGroupPhoto)
+    PhotoEditView(photo: photo)
 }
---
+++ Morume/MoruMe/Features/PhotoEdit/PhotoEditViewModel.swift
@@ -0,0 +1,60 @@
+//
+//  PhotoEditViewModel.swift
+//  MoruMe
+//
+//  Created by 青原光 on 2025/05/29.
+//
+
+import Observation
+import SwiftUI
+import UIKit
+
+@MainActor
+@Observable
+final class PhotoEditViewModel {
+    private let photoEditRepository: PhotoEditRepository
+    private let filterRepository: FilterRepository
+    private let userRepository: UserRepository
+    private let originalPhoto: UIImage
+    private let faceColors: [Color] = [.red, .orange, .yellow, .green, .blue]
+
+    var editPhoto: UIImage
+    var detectedFaces: [DetectedFace] = []
+
+    var isProcessing = false
+
+    init(originalImage: UIImage) {
+        self.originalPhoto = originalImage
+        self.editPhoto = originalImage
+        self.photoEditRepository = PhotoEditRepository(originalPhoto: originalImage)
+        self.filterRepository = MoruMeAPIFilterRepository()
+        self.userRepository = MoruMeAPIUserRepository()
+    }
+
+    func detectFaceLandmarks() async {
+        isProcessing = true
+        defer {
+            isProcessing = false
+        }
+
+        do {
+            try await photoEditRepository.detectFaceAndLandmarks()
+
+            detectedFaces = []
+            for index in 0..<photoEditRepository.detectedFaceMeshes.count {
+                if index >= faceColors.count {
+                    break
+                }
+                let detectedFace = DetectedFace(
+                    faceRegion: photoEditRepository.detectedFaceRegions[index],
+                    color: faceColors[index],
+                    user: nil,
+                    faceMesh: photoEditRepository.detectedFaceMeshes[index]
+                )
+                detectedFaces.append(detectedFace)
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
