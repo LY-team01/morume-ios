068903332603bacf780100a3e633db74575d0917
Hikaru Aohara <hikaruaohara@icloud.com>
2025-05-30 05:01:22 +0900

    UserSelectViewModelを実装

---
+++ Morume/MoruMe/Features/PhotoEdit/UserSelection/UserSelectViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  UserSelectionViewModel.swift
+//  MoruMe
+//
+//  Created by 青原光 on 2025/05/30.
+//
+
+import Observation
+
+@MainActor
+@Observable
+final class UserSelectViewModel {
+    private let userRepository: UserRepository
+
+    var allUsers: [User] = []
+    var isProcessing = false
+
+    init() {
+        userRepository = MoruMeAPIUserRepository()
+    }
+
+    func fetchAllUsers() async throws {
+        isProcessing = true
+        defer { isProcessing = false }
+
+        allUsers = try await userRepository.fetchUsers()
+    }
+}
